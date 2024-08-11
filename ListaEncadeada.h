65003ccf2fc28e015f61d6a8ccae59bfef6ac312
Caio <101272610+KYOgomes@users.noreply.github.com>
2024-08-11 18:11:37 -0300

    Update and rename LinkedList.h to ListaEncadeada.h

---
+++ ListaEncadeada.h
@@ -0,0 +1,22 @@
+#ifndef LISTAENCADEADA_H
+#define LISTAENCADEADA_H
+
+struct No 
+{
+    int dado;
+    No* proximo;
+};
+
+class ListaEncadeada {
+public:
+    ListaEncadeada();
+    void adicionar(int valor);
+    bool encontrar(int valor);
+    void remover(int valor);
+    void removerPrimeirosK(int k);
+
+private:
+    No* cabeca;
+};
+
+#endif // LISTAENCADEADA_H
