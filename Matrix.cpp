50e4f1ae556e2edf5adf29e80b0c08cee677ad5d
RafaelMaluf <104031822+RafaelMaluf@users.noreply.github.com>
2024-08-11 18:10:23 -0300

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,19 +1,20 @@
 #include "Matrix.h"
+using namespace std;
 
 Matrix::Matrix(int rows, int cols) : rows(rows), cols(cols) {
-    matrix.resize(rows, std::vector<int>(cols, 0));
+    matrix.resize(rows, vector<int>(cols, 0));
 }
 
-void Matrix::set(int row, int col, int value) {
+void Matrix::set(int row, int col, int valor) {
     if (row >= 0 && row < rows && col >= 0 && col < cols) {
-        matrix[row][col] = value;
+        matrix[row][col] = valor;
     }
 }
 
-bool Matrix::find(int value) {
+bool Matrix::find(int valor) {
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
-            if (matrix[i][j] == value) {
+            if (matrix[i][j] == valor) {
                 return true;
             }
         }
